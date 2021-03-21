5bc1610279f7705558c01c6480e341c027a709df
eyup.kucuksari <80711644+eyup-ks@users.noreply.github.com>
2021-03-22 00:31:26 +0100

    Add files via upload

---
+++ Day_3_questions.java
@@ -0,0 +1,71 @@
+package day3;
+
+public class Day_3_questions {
+
+	public static void main(String[] args) {
+System.out.println("question "+1);
+		int number=5;
+		
+		System.out.println(number);
+		System.out.println();
+System.out.println("question "+2);
+	
+		String message = "c";
+		System.out.println(message);
+		System.out.println();
+System.out.println("question "+3);
+		int num=10;
+		double cost=20;
+		
+		System.out.println(num+" "+cost);
+		System.out.println();
+System.out.println("question "+4);		
+        String word="dream";
+        System.out.println("Today's Word-Of-The-Day is: "+ word);
+        System.out.println();
+System.out.println("question "+5);
+        int first= 120;
+		int second= 99;
+		
+		System.out.println("first is "+first+" second = "+second);
+		System.out.println();
+System.out.println("question "+6);
+        String name1="James";
+		String name2="Jill";
+		String name3="Betty";
+		String name4="Herb";
+		String name5="Bob";
+		
+		System.out.println(name1+", "+name3+", "+name4+", "+name2+", "+name5);
+		System.out.println();
+
+System.out.println("question "+7);
+    	int i=9;
+    	float f=33.4f;
+    	
+    	System.out.println("i="+i+" f="+f);
+    	System.out.println();
+
+System.out.println("question "+8);
+		String lan1="Java";
+		String lan2="SQL";
+		
+		System.out.println("I will learn \""+lan1+"\" and \""+lan2+"\" at CybertekSchool.");
+		System.out.println();
+
+System.out.println("question "+9);
+        byte steps=100;
+		short miles=5000;
+		int count=1_000_000;
+		long population=3_434_455_667l;
+		System.out.println();
+System.out.println("question "+10);
+        
+
+
+
+
+
+	}
+
+}
